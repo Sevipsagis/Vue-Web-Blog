eb607097a1a55d3f9f6f6a7eb4d3fc201609d77f
Unknown <sevipsagis@gmail.com>
2019-04-04 08:09:35 +0700

    add body-parser

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // get status
 app.get("/status", (req, res) => {
@@ -11,21 +16,6 @@ app.get("/hello/:person", (req, res) => {
     res.send("Hello " + req.params.person);
 });
 
-// create user
-app.post("/user", (req, res) => {
-    res.send("create user");
-});
-
-// edit user
-app.put("/user/:userId", (req, res) => {
-    res.send("edit user " + req.params.userId);
-});
-
-// delete user
-app.delete("/user/:userId", (req, res) => {
-    res.send("delete user " + req.params.userId);
-});
-
 // gel all users
 app.get("/users", (req, res) => {
     res.send("get all users");
@@ -34,6 +24,21 @@ app.get("/users", (req, res) => {
 // gel user
 app.get("/user/:userId", (req, res) => {
     res.send("get user " + req.params.userId);
+});
+
+// create user
+app.post("/user", (req, res) => {
+    res.send("create user : " + JSON.stringify(req.body));
+});
+
+// edit user
+app.put("/user/:userId", (req, res) => {
+    res.send("edit user " + req.params.userId + " : " + JSON.stringify(req.body));
+});
+
+// delete user
+app.delete("/user/:userId", (req, res) => {
+    res.send("delete user " + req.params.userId + " : " + JSON.stringify(req.body));
 });
 
 app.listen(8081, () => {
