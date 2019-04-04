e18ff2cf45f9391e39a52b8dea4ff1d06d755fb2
Unknown <sevipsagis@gmail.com>
2019-04-04 07:25:29 +0700

    add routing path

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
 
+// get status
 app.get("/status", (req, res) => {
-    res.send("Hello, World!!");
-})
+    res.send("App running on 8081");
+});
+
+//test req
+app.get("/hello/:person", (req, res) => {
+    res.send("Hello " + req.params.person);
+});
+
+// create user
+app.post("/user", (req, res) => {
+    res.send("create user");
+});
+
+// edit user
+app.put("/user/:userId", (req, res) => {
+    res.send("edit user");
+});
+
+// delete user
+app.delete("/user/:userId", (req, res) => {
+    res.send("delete user");
+});
+
+// gel all users
+app.get("/users", (req, res) => {
+    res.send("get all users");
+});
+
+// gel user
+app.get("/user/:userId", (req, res) => {
+    res.send("get user");
+});
 
 app.listen(8081, () => {
     console.log("server running on 8081");
